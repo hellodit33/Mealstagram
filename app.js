eb5f19c83426a9cee54f24b81c34943bf577a19d
964Code <964code@gmail.com>
2021-12-18 19:24:31 +0100

    Added missing icon for navbar. Removed navbar JS.code from script and added it as separate code below. Navbar dropdown did not work since it was placed at the start of the HTML.

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-window.addEventListener('load', function () {
-    document.querySelector('input[type="file"]').addEventListener('change', function () {
-        if (this.files && this.files[0]) {
-            var img = document.querySelector('#myImg');
-            img.onload = () => {
-                URL.revokeObjectURL(img.src);  // no longer needed, free memory
-            }
+window.addEventListener("load", function () {
+  document
+    .querySelector('input[type="file"]')
+    .addEventListener("change", function () {
+      if (this.files && this.files[0]) {
+        var img = document.querySelector("#myImg");
+        img.onload = () => {
+          URL.revokeObjectURL(img.src); // no longer needed, free memory
+        };
 
-            img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+        img.src = URL.createObjectURL(this.files[0]); // set src to blob url
 
-            document.querySelector('#myImg').style.display = "block";
-        }
+        document.querySelector("#myImg").style.display = "block";
+      }
     });
 });
 
 //skapar en klass meal
 class Meal {
-    constructor(postname, img, diet, food, recipe) {
-        /*this.image = image;*/
-        this.postname = postname;
-        this.img = img;
-        this.diet = diet;
-        this.food = food;
-        this.recipe = recipe;
+  constructor(postname, img, diet, food, recipe) {
+    /*this.image = image;*/
+    this.postname = postname;
+    this.img = img;
+    this.diet = diet;
+    this.food = food;
+    this.recipe = recipe;
+  }
 
-    }
-
-
-    /*en toString metod som returnerar info om objektet
+  /*en toString metod som returnerar info om objektet
 
     toString() {
         if (this.image)
@@ -35,93 +35,75 @@ class Meal {
             return "Please upload an image";
     }*/
 
-    toString() {
-
-        return this.postname + " " + this.img + " " + this.diet + " " + this.food + " " + this.recipe;
-
-
-    }
-
-
+  toString() {
+    return (
+      this.postname +
+      " " +
+      this.img +
+      " " +
+      this.diet +
+      " " +
+      this.food +
+      " " +
+      this.recipe
+    );
+  }
 }
 
 //skapa en array meal för att lägga alla måltidsobjekt i den
 
 let meal = [];
-let postname = document.getElementById('postnameInput').value;
-let img = document.getElementById('imageInput').value;
+let postname = document.getElementById("postnameInput").value;
+let img = document.getElementById("imageInput").value;
 let diet = document.getElementById("dietInput").value;
 let food = document.getElementById("foodInput").value;
 let recipe = document.getElementById("recipeInput").value;
 
 //for loop to create objects
 for (let i = 0; i < 1; i++) {
-    meal.push(new Meal(postname[i], img[i], diet[i], food[i], recipe[i]));
+  meal.push(new Meal(postname[i], img[i], diet[i], food[i], recipe[i]));
 }
-
-
 
 //göra en lista av info om maten
 function addImageToTheFeed() {
-    /*let newImage = document.getElementById("image").img.src;*/
-    let postname = document.getElementById('postnameInput').value;
-    let img = document.getElementById('imageInput').value;
-    let diet = document.getElementById("dietInput").value;
-    let food = document.getElementById("foodInput").value;
-    let recipe = document.getElementById("recipeInput").value;
+  /*let newImage = document.getElementById("image").img.src;*/
+  let postname = document.getElementById("postnameInput").value;
+  let img = document.getElementById("imageInput").value;
+  let diet = document.getElementById("dietInput").value;
+  let food = document.getElementById("foodInput").value;
+  let recipe = document.getElementById("recipeInput").value;
 
-
-
-
-
-    /*varna användaren att vi behöver bilden, dieten, och typen av mat
+  /*varna användaren att vi behöver bilden, dieten, och typen av mat
     if (!newImage || !newDiet || !newFood) {
         output.innerHTML = "Please upload an image, and choose a diet as well as a type of food.";
         return;
     }*/
 
+  //pusha infon i ett objekt literal för varje måltid
+  meal.push(new Meal(postname, img, diet, food, recipe));
 
-    //pusha infon i ett objekt literal för varje måltid
-    meal.push(new Meal(postname, img, diet, food, recipe));
+  document.getElementById("foodInput").value = "";
+  document.getElementById("dietInput").value = "";
+  document.getElementById("imageInput").value = "";
+  document.getElementById("recipeInput").value = "";
 
-
-    document.getElementById('foodInput').value = '';
-    document.getElementById('dietInput').value = '';
-    document.getElementById('imageInput').value = '';
-    document.getElementById('recipeInput').value = '';
-
-
-    //anropa funktionen writeAllMeals()
-    writeAllMeals();
-
-
+  //anropa funktionen writeAllMeals()
+  writeAllMeals();
 }
 
 //skriva ut infon om måltiden
 
 function writeAllMeals() {
-    let postname = document.getElementById('postnameInput').value;
+  let postname = document.getElementById("postnameInput").value;
 
-    let img = document.getElementById('imageInput').value;
-    let diet = document.getElementById("dietInput").value;
-    let food = document.getElementById("foodInput").value;
-    let recipe = document.getElementById("recipeInput").value;
+  let img = document.getElementById("imageInput").value;
+  let diet = document.getElementById("dietInput").value;
+  let food = document.getElementById("foodInput").value;
+  let recipe = document.getElementById("recipeInput").value;
 
+  document.getElementById("output").innerHTML = "";
 
-
-
-    document.getElementById("output").innerHTML = "";
-
-
-
-
-    for (let i = 0; i < meal.length; i++) {
-        document.getElementById("output").innerHTML += meal[i].toString() + "<br>";
-    }
-
+  for (let i = 0; i < meal.length; i++) {
+    document.getElementById("output").innerHTML += meal[i].toString() + "<br>";
+  }
 }
-
-
-
-
-
--- script.js
+++ script.js
@@ -1,78 +1,64 @@
-
-
 /* class for profile objects */
 class Profile {
-    constructor(name, diet, age, food, img) {
-        this.name = name;
-        this.diet = diet;
-        this.age = age;
-        this.food = food;
-        this.img = img;
-    }
+  constructor(name, diet, age, food, img) {
+    this.name = name;
+    this.diet = diet;
+    this.age = age;
+    this.food = food;
+    this.img = img;
+  }
 }
 
 /* arrays for object properties */
-let names = ['Swedish Chef'];
-let diets = ['Omnivore'];
-let ages = ['61'];
-let foods = ['Meatballs'];
-let imgs = ['img/profile pic.jpg']
+let names = ["Swedish Chef"];
+let diets = ["Omnivore"];
+let ages = ["61"];
+let foods = ["Meatballs"];
+let imgs = ["img/profile pic.jpg"];
 
 /* array for objects */
 let profiles = [];
 
 /* for loop to create objects */
-for (let i = 0; i <1; i++) {
-    profiles.push(new Profile(names[i], diets[i], ages[i], foods[i], imgs[i]));
-}   
-    
+for (let i = 0; i < 1; i++) {
+  profiles.push(new Profile(names[i], diets[i], ages[i], foods[i], imgs[i]));
+}
+
 /* displays object properties in img and p tags */
-window.addEventListener('load', (event) => {
+window.addEventListener("load", (event) => {
+  /* displays object properties in p tag */
+  nameDisplay = document.getElementById("nameDisplay");
+  nameDisplay.innerHTML = names.toString();
 
-    /* displays object properties in p tag */
-    nameDisplay = document.getElementById('nameDisplay');
-    nameDisplay.innerHTML = names.toString();
+  /* displays object properties in p tag  */
+  dietDisplay = document.getElementById("dietDisplay");
+  dietDisplay.innerHTML = diets.toString();
 
-    /* displays object properties in p tag  */
-    dietDisplay = document.getElementById('dietDisplay');
-    dietDisplay.innerHTML = diets.toString();
+  /* displays object properties in p tag  */
+  ageDisplay = document.getElementById("ageDisplay");
+  ageDisplay.innerHTML = ages.toString();
 
-    /* displays object properties in p tag  */
-    ageDisplay = document.getElementById('ageDisplay');
-    ageDisplay.innerHTML = ages.toString();
+  /* displays object properties in p tag  */
+  foodDisplay = document.getElementById("foodDisplay");
+  foodDisplay.innerHTML = foods.toString();
 
-    /* displays object properties in p tag  */
-    foodDisplay = document.getElementById('foodDisplay');
-    foodDisplay.innerHTML = foods.toString();
-
-    /* displays object properties in img tag with variable */
-    var profilePics = document.getElementById('profilePics');
-    profilePics.src = imgs.toString();
-   
+  /* displays object properties in img tag with variable */
+  var profilePics = document.getElementById("profilePics");
+  profilePics.src = imgs.toString();
 });
-
 
 /* function to change stylesheet and css styling with button */
 function toggleTheme() {
-    // obtain an array of all <link> elements.
-    var theme = document.getElementsByTagName('link')[0];
-    var img = document.getElementById('profile');
+  // obtain an array of all <link> elements.
+  var theme = document.getElementsByTagName("link")[0];
+  var img = document.getElementById("profile");
 
-    // changes value of link href to change stylesheet
-    if (theme.getAttribute('href') == 'styleprofile.css') {
-        theme.setAttribute('href', 'stylechange.css');
-        img.src = 'img/profile2.png'
-    } else {
-        theme.setAttribute('href', 'styleprofile.css');
-        img.src = 'img/profile.png'
-    }
+  // changes value of link href to change stylesheet
+  if (theme.getAttribute("href") == "styleprofile.css") {
+    theme.setAttribute("href", "stylechange.css");
+    img.src = "img/profile2.png";
+  } else {
+    theme.setAttribute("href", "styleprofile.css");
+    img.src = "img/profile.png";
+  }
 }
-
-
-const toggleDropdown = document.querySelector(".sub-links.container");
-console.log(toggleDropdown);
-const menu = document.querySelector(".drop-down.icon");
-
-menu.addEventListener("click", () => {
-  toggleDropdown.classList.toggle("show");
-});
